9406de911599dd0721d908469c1b694dab4deeb3
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-06-05 18:22:59 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import s from './App.module.css';
+import { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export class App extends Component {
+  state = {
+    searchData: '',
+  };
+
+  onSubmit = searchData => {
+    if (searchData.trim() === '') {
+      return toast.error('Enter the meaning for search');
+    } else if (searchData === this.state.searchData) {
+      return;
+    }
+    this.setState({ searchData: searchData });
+  };
+
+  render() {
+    const { onSubmit } = this;
+    const { searchData } = this.state;
+    return (
+      <div className={s.App}>
+        <Searchbar onSubmit={onSubmit} />
+        <ImageGallery searchData={searchData} />
+        <ToastContainer autoClose={2500} />
+      </div>
+    );
+  }
+}
